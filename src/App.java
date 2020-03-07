318ea80fb2805702a3410d9d75fe0edf583c77e2
Andrey Antochevis <andrey.antochevis@gmail.com>
2020-03-07 12:15:00 -0300

    repovendo mensagens estranhas

--- src/App.java
+++ src/App.java
@@ -2,10 +2,5 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("teste");
-        System.out.println("teste2");
-        System.out.println("teste3");
-        System.out.println("teste4");
-
     }
 }
