7cd57915202c866958a7bf2126fdedafb9979a74
Andrey Antochevis <andrey.antochevis@gmail.com>
2020-03-07 10:58:03 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("é os gu do inter");
+        System.out.println("ggwp");
+        System.out.println("Colorado");
+        System.out.println("aloha");
+
+    }
+}
