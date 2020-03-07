be8cb3d7bde100ac49cad93bd7eae579d1df1003
Andrey Antochevis <andrey.antochevis@gmail.com>
2020-03-07 15:31:17 -0300

    calculadora

--- src/App.java
+++ src/App.java
@@ -1,6 +1,20 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Calculadora calculadoraDeIdades = new Calculadora();
+        int somaDasIdades = calculadoraDeIdades.somar(33, 54);
+        System.out.println(somaDasIdades);
+
+        int subtracaoDasIdades = calculadoraDeIdades.subtrai(66, 22);
+        System.out.println(subtracaoDasIdades);
+
+        int multiplicacaoDasIdades = calculadoraDeIdades.multiplica(23, 3);
+        System.out.println(multiplicacaoDasIdades);
+
+        int divisaoDasIdades = calculadoraDeIdades.divide(99, 33);
+        System.out.println(divisaoDasIdades);
     }
 }
